9ab3d125f0d283a720683281d0e4fd86504ecd1c
Gustavo Fernandez <sr1176626173@gmail.com>
2024-01-17 14:47:17 -0600

    Primeros pasos de follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,11 +1,40 @@
+// importar modelo
+const Follow = require("../models/follow");
+const User = require("../models/user");
+
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
-  return res.status(200).send({
-    message: "Mensaje enviado desde: controllers/follow.js",
-  });
+    return res.status(200).send({
+        message: "Mensaje enviado desde: controllers/follow.js",
+    });
 };
+
+// Accion de guardar un follow ( accion seguir)
+const save = (req, res) => {
+
+    // Conseguir datos por body
+
+    // savar id del usuraio identificado
+
+    // Crear objeto con modelo follow
+
+    // Guardar objeto en base de datos
+
+    return res.status(200).send({
+        status: "success",
+        message: "accion save",
+        idenuty: req.user
+    });
+}
+
+// Acccion de borrar un follow (accion de seguir)
+
+// Accion listado de usuarios que estoy siguiendo
+
+// Acccion listado de usuario que me siguen
 
 // Exportar acciones
 module.exports = {
-  pruebaFollow,
+    pruebaFollow,
+    save
 };
--- routers/follow.js
+++ routers/follow.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const FollowController = require("../controllers/follow")
+const FollowController = require("../controllers/follow");
+const check = require("../middlewares/auth");
 
 // Definir rutas
 router.get('/pruebas-follow', FollowController.pruebaFollow);
-
+router.post('/save', check.auth, FollowController.save)
 // Exportar router
 module.exports = router;
